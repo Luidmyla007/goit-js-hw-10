9b34faa7786cfbb9c5179a01dae8b328c7b817f4
Liudmyla007 <ldatsenko3@gmail.com>
2023-01-07 21:56:57 +0200

    constants

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+const myInput = document.querySelector("#search-box");
+myInput.style.marginTop = "40px";
+myInput.style.marginLeft = "40px";
+// myInput.style.borderColor = "Blue";
+const myList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+
+
+
+
