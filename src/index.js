5179e4fe6cb3b605a6482f0f1b4d033bec93af79
Liudmyla007 <ldatsenko3@gmail.com>
2023-01-12 19:49:05 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,114 +1,77 @@
-import './css/styles.css';
-import Notiflix from 'notiflix';
-import debounce from 'lodash.debounce';
-const DEBOUNCE_DELAY = 300;
+import './css/styles.css'
+import debounce from 'lodash.debounce'
+import Notiflix from 'notiflix'
+import { fetchCountries } from './fetchCountries.js'
 
-const myInput = document.querySelector("#search-box");
-myInput.style.marginTop = "40px";
-myInput.style.marginLeft = "40px";
-// myInput.style.borderColor = "Blue";
-const myList = document.querySelector(".country-list");
-const countryInfo = document.querySelector(".country-info");
+const DEBOUNCE_DELAY = 300
 
-myInput.addEventListener("input", debounce(onSearch, DEBOUNCE_DELAY));
-function onSearch(){
-  const name = myInput.value;
-   console.log(name);
+const countryInput = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
+
+countryInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
+
+function onSearch() {
+  const name = countryInput.value.trim()
+  if (name === '') {
+    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+  }
+
   fetchCountries(name)
-  .then((data) => console.log(data))
-  .catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
+    .then(data => {
+      countryList.innerHTML = ''
+      countryInfo.innerHTML = ''
+      if (data.length === 1) {
+        countryList.insertAdjacentHTML('beforeend', renderList(data))
+        countryInfo.insertAdjacentHTML('beforeend', renderInfo(data))
+      } else if (data.length >= 10) {
+          tooManyMatches()
+      } else {
+        countryList.insertAdjacentHTML('beforeend', renderList(data))
+      }
+      })
+      .catch(wrongName)
+    }
+
+function renderList(data) {
+  const markup = data
+    .map(({ name, flags }) => {
+      return `
+          <li class="country-list__item">
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <h2 class="country-list__name">${name.official}</h2>
+          </li>
+          `
+    })
+    .join('')
+  return markup
 }
 
-function longResponce() {
-  const a = 10;
-  if (data.length > a) {
+function renderInfo(data) {
+  const markup = data
+    .map(({ capital, population, languages }) => {
+      return `
+        <ul class="country-info__list">
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+        </ul>
+        `
+    })
+    .join('')
+  return markup
+}
+
+
+function wrongName() {
+  Notiflix.Notify.failure('Oops, there is no country with that name')
+}
+
+function tooManyMatches() {
   Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
 }
-}
 
 
-function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v3.1/name/';
-
-    return fetch(`${BASE_URL}${name}`).then(
-      (response) => {
-        if (!response.ok) {
-        throw new Error(Notiflix.Notify.failure('Oops, there is no country with that name'));         
-      }      
-      return response.json();
-    }
-  );
-}
-
-fetchCountries()
-  .then((data) => console.log(data))
-  
-  // if (data.length > 10) {
-  //   Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-  // })
-  
-    .catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
-
-
-
-// fetchCountries()
-//     .then((data) => renderCountryList(name))
-//     .catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
-
-
-    // function renderCountryList(name) {
-//   console.log("my markup");
-  // const markup = countries;
-  //   .map((country) => {
-  //     return `<li>
-  //         <p><b>Name</b>: ${user.name}</p>
-  //         <p><b>Email</b>: ${user.email}</p>
-  //         <p><b>Company</b>: ${user.company.name}</p>
-  //       </li>`;
-  //   })
-  //   .join("");
-  // userList.innerHTML = markup;
-// }
-
-
-
-// fetchUsersBtn.addEventListener("click", () => {
-//   fetchUsers()
-//     .then((users) => renderUserList(users))
-//     .catch((error) => console.log(error));
-// });
-
-// function fetchUsers() {
-//   return fetch("https://jsonplaceholder.typicode.com/users").then(
-//     (response) => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     }
-//   );
-// }
-
-// function renderUserList(users) {
-//   const markup = users
-//     .map((user) => {
-//       return `<li>
-//           <p><b>Name</b>: ${user.name}</p>
-//           <p><b>Email</b>: ${user.email}</p>
-//           <p><b>Company</b>: ${user.company.name}</p>
-//         </li>`;
-//     })
-//     .join("");
-//   userList.innerHTML = markup;
-// }
-
-
-
-
-// onClose(selectedDates) {
-//     const selectDate = selectedDates[0];
-//     // если выбрана дата из прошлого выводим предупрежд
-//       if (date.getTime() >= Number(selectDate.getTime())) {
-//         Notiflix.Notify.failure('Please choose a date in the future');  
-//         return
-//     };
+// styles
+countryInput.style.marginTop = "40px";
+countryInput.style.marginLeft = "40px";
